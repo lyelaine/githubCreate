c1a834b359f45f13da95b4764508ead433cbb430
lyelaine <834786140@qq.com>
2016-06-24 23:20:36 +0800

    creat new project,commit

---
+++ demo/demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by elaine on 2016/6/24.
+//  Copyright © 2016年 elaine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demo
+//
+//  Created by elaine on 2016/6/24.
+//  Copyright © 2016年 elaine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
